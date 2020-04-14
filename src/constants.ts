15e97c623ff1c43d6fd6d006a92c01d71ef3f16d
Patrick Desjardins <pdesjardins3@gatech.edu>
2020-04-13 22:39:59 -0700

    Floor plan in SVG and color acceptable by a constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const TEXT_COLOR = "rgba(45,62,69,1)";
-export const LINE_COLOR = "rgba(45,62,69,1)";
+export const LINE_COLOR = "rgba(65, 40, 150, 1)";
 
-export const WALL_COLOR = "red";
+export const WALL_COLOR = "rgb(55, 95, 120)";
 
 export const LIGHT_ON_COLOR = "rgb(255, 223, 90)";
 export const LIGHT_OFF_COLOR = "rgba(115, 105, 45, 0.5)";
