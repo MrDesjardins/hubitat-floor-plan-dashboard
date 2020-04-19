47d4209f9107d13ea908fc4c9e03067c12d3acab
Patrick Desjardins <pdesjardins3@gatech.edu>
2020-04-18 21:08:17 -0700

    Change Color Palette

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
-export const TEXT_COLOR = "rgba(45,62,69,1)";
-export const LINE_COLOR = "rgba(65, 40, 150, 1)";
+export const TEXT_COLOR = "#3B829C";
+export const LINE_COLOR = "#D7E7D0";
 
-export const WALL_COLOR = "rgb(55, 95, 120)";
+export const WALL_COLOR = "#3B829C";
 
-export const LIGHT_ON_COLOR = "rgb(255, 223, 90)";
-export const LIGHT_OFF_COLOR = "rgba(115, 105, 45, 0.5)";
+export const LIGHT_ON_COLOR = "rgba(255, 223, 90, 1)";
+export const LIGHT_OFF_COLOR = "rgba(255, 223, 90, 0.1)";
 
-export const FLOOR_COLOR = "rgb(40, 50, 75)";
+export const FLOOR_COLOR = "#2C435B";
 
 export const MENU_BACKGROUND_COLOR = "rgb(16, 92, 131)";
 export const MENU_TEXT_COLOR = "rgb(255, 255, 255)";
